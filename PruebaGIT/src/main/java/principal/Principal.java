06d0dfee2c366ea30e61b54566ad4227840894ad
User <User@DESKTOP-5O0AJIR>
2023-12-12 19:24:36 +0100

    Clase principal

--- PruebaGIT/src/main/java/principal/Principal.java
+++ PruebaGIT/src/main/java/principal/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Comienzo");
+		System.out.println("Añado funcionalidades");
+		System.out.println("Final");
 	}
 
 }
