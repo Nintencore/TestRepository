8a71e87194a20d6de7babc81003b47edd230dfca
User <User@DESKTOP-5O0AJIR>
2023-12-12 19:18:25 +0100

    Interfaces y modelo

---
+++ PruebaGIT/src/main/java/interfaces/interfaz.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface interfaz {
+	
+	public int devuelveint();
+
+}
---
+++ PruebaGIT/src/main/java/modelo/miclase.java
@@ -0,0 +1,17 @@
+package modelo;
+import interfaces.interfaz;
+
+public class miclase  implements interfaz{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int devuelveint() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
